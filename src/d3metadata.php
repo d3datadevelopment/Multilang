11b2a2eeef08027587fa1ca3a8e371b03d0d1941
Daniel Seifert <ds@shopmodule.com>
2021-01-07 10:17:43 +0100

    add 4.1.0.2 (ionCube for PHP 7.1 - 7.4)

--- src/d3metadata.php
+++ src/d3metadata.php
@@ -1,16 +1,17 @@
 <?php //006e9
-// This Software is the property of D³ Data Development and is protected by copyright law - it is NOT Freeware.  Any unauthorized use of this software without a valid license key is a violation of the license agreement and will be prosecuted by civil and criminal law.  Inhaber: Thomas Dartsch Alle Rechte vorbehalten  @package Multilang @version 4.1.0.1 PHP71 (08.12.2020) @author  Daniel Seifert support@shopmodule.com @copyright (C) 2020, D3 Data Development @see http://www.shopmodule.com
+// This Software is the property of D³ Data Development and is protected by copyright law - it is NOT Freeware.  Any unauthorized use of this software without a valid license key is a violation of the license agreement and will be prosecuted by civil and criminal law.  Inhaber: Thomas Dartsch Alle Rechte vorbehalten  @package Multilang @version 4.1.0.2 PHP71 (07.01.2021) @author  Daniel Seifert support@shopmodule.com @copyright (C) 2021, D3 Data Development @see http://www.shopmodule.com
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
 ?>
-HR+cPqcEk9LVLm7Q9ZijjZ71CyOrrOja6Qy7/RAuezvuLLujcAI573GSLkJt5tfSzGUalzq7+XFQ
-FIcBrZuUMQuHmdGTWCNe8ax7gE0hoTd+m9UYg4Vj27Vs6qAVur4PwZi27V6+/KRflUeRB2Jl1mPQ
-geD3hjOxE6WMztF+Qg0zcVokgKC/ZCV1LIsUB0T/svu+68a9si7xN0xyUvi+O0nHRdRysAKT1FnQ
-4CTvgXtgjEegceR+in9VgL1CzyPmY4AsqOguYujr+5pYWhpmctBDljR3/YbiY9LqBk0zMeNrfads
-rz0kwVXvWuBUVKP1J+OQyqXOa88XgtK+PMhb8EVQE9oPBckcCo/rnDRT2lTJEG2gOrG/UgObdSWz
-+E0Rj+a77/Z/csC2GX7oEqOtG0FjrvxjlRDmKAhvndWomGVyKygLZQFrEVIkarOAaxaNfe5iO7Rc
-y0pnZrioOv7qT0dKrshcm7fwtxdgRni+q92IbC4rAU9hgmCcDbADp7dWZ8zLOZqH7GtQb+chd4p0
-AFl0jS7Xfp997cH4ujjwH+brrBqDHnvRlHUtezWnwhymZ9yrCGrBQZOAcK/Bhp/VmO+k0MQjojG2
-c1Z3FmsK+tk5ZW035QzJIAP/Wymkt0A2FP/zyLLdeF5yjNcoddgaJiLqi/Hm/mwZh4ZQiZ+086GE
-9T8kwutkL0sIVp+DlMHPqrq86CDIsdiCMjjbE8fUE4kVqnFYRsLXTvJHEuvrsLH1HFhPKbdU1ne0
-pp9sopaWDBOwHisIYXRvl6VM9uHRngBwPXFamqpP6b2MIMfrMDC1ZC5MXDPaedv8HV9t4NINCMgi
-PAcDw/GmLIbSa3xDI6zf5yK7Lmj97EfoSv9G3bXLuzVBUkgk1ZTcqMtTOAR4sLWv
+HR+cPyFEx/ZQ5zJ/MQDxUjkAKS+TcyS6fOO4IfwuDpkYROdyBYFlgX9PU2/3lF0CNXgg6mxn3hGz
+gJ622VClhUJMiDoLl6IbX3+dghBjFpab1pUhB5N/4l/099fjMIgfN5fdLn2PmTiRRLPzMK/g6CUK
+LnnHh4aQKycwXdU7BPE2zQRCEKIx/SwqTZtyX1sd/RlUv44rWRD0ygI6kzD2A8pq7bxRVWeuWYS3
+gSNRvRzvu+jICyEOTfRwnwPCvwlkkeudbobp/VltxGpCX7oUsWUnvr7oDvPlWzPeK3QuF1nehej0
+cT0/HxjYUntMbIcOszTaXn0uHs7pW/rk8s8NvbUH0r+aIFwAVBxWHfgE9EwL4RzuxaycM2AQKW4r
+bRniiJAXe0DIzz9ibUjd5l3GdumRBA1JHvNPhS4mHAbxN1dcJfjf0XxMUJbSQk21SzQD3bhZ8wqA
+AwR+GoKVzlt0csieBvlUg6gO3N1VUO5WNmSDv05eyiDPj4N8bY9yCeP7Vzsaz6AHzl3jkKpF5ewf
+WDdIY5i3MYEj+G+Oz6w0Kv1mGPkWWEUmFg6WFLnNNMrMYQI9sY4zGy6dipJBGZdIIjv/zIAfxmxA
+MprJ8IxJ8vhuhGkguboq8bnSmndAHDT/7yY6I7Ysgrq9MZfjQJqAV4exDwoDGxt1v35JyGhfgx4L
+BP4DrnWo/c5bCUYpSDhZ4Y+g9VXlK8hAPi35yTycT7yVSwU6KwoZcGqVpDsAGdKTM3N86n6BOyjZ
+787hRuyaWuiR6MzyNwjohNlsUCcLIJPPbUIL6bHlmlusoW1xryGaZ1LrNifW4FIIdUuXQkWBtGFm
+5VBFGQxe366Mrk62xjDGv1WsiMHtV6I7JnS8rK5WjveUn2W6j+yKjOKeyFCXiHguUzH1YyiW6qIx
+GTmcD0==
